480e584faa56168e43a5360ef09663ecb66c773e
AngadSudan <angadsudan453@gmail.com>
2026-07-30 23:33:08 +0530

    [BACKEND] - tried deploying to vercel

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -126,13 +126,13 @@ app.get("/health", async (req, res) => {
   });
 });
 app.use("/api/v1/auth", authRouter);
-declare global {
-  namespace Express {
-    interface Request {
-      user?: JwtPayload;
-    }
-  }
-}
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       user?: User | JwtPayload;
+//     }
+//   }
+// }
 // routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/interviewers", interviewerRouter);
